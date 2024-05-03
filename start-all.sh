696704005d6d41d33968830a7e5c7339e98add12
joamona <joamona@cgf.upv.es>
2024-05-03 11:38:42 +0200

    Cambiado docker-compose por docker compose

--- start-all.sh
+++ start-all.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo "### Starting PostGIS. Port 5440"
-docker-compose -f postgis/docker-compose.yml up -d
+docker compose -f postgis/docker-compose.yml up -d
 echo
 echo "### Starting GeoServer. Port 8083"
-docker-compose -f geoserver/docker-compose.yml up -d
+docker compose -f geoserver/docker-compose.yml up -d
 echo
 echo "### Starting PgAdmin4. Port 5050"
-docker-compose -f pgadmin4/docker-compose.yml up -d
+docker compose -f pgadmin4/docker-compose.yml up -d
 echo Hecho
--- stop-all-prod.sh
+++ stop-all-prod.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo "### Stopping PostGIS. Port 5432"
-docker-compose -f postgis/docker-compose.prod.yml stop
+docker compose -f postgis/docker-compose.prod.yml stop
 echo
 echo "### Stopping GeoServer. Port 8080"
-docker-compose -f geoserver/docker-compose.prod.yml stop
+docker compose -f geoserver/docker-compose.prod.yml stop
 echo
 echo "### Stopping PgAdmin4. Port 5050"
-docker-compose -f pgadmin4/docker-compose.prod.yml stop
+docker compose -f pgadmin4/docker-compose.prod.yml stop
 echo Done
