37dfff4dfd3612d4f7d96622c22fa35fca390869
joamona <joamona@cgf.upv.es>
2024-05-03 09:53:56 +0200

    Añadidos ficheros para producción

---
+++ stop-all.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "### Stopping PostGIS"
+docker-compose -f postgis/docker-compose.yml stop
+echo
+echo "### Stopping GeoServer"
+docker-compose -f geoserver/docker-compose.yml stop
+echo
+echo "### Stopping PgAdmin4"
+docker-compose -f pgadmin4/docker-compose.yml stop
+echo Done
